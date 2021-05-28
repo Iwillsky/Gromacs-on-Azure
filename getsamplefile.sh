947f7e77c981487c20993dd12098a73752c83039
iwillsky <45908192+Iwillsky@users.noreply.github.com>
2021-05-28 10:54:50 +0800

    push0528
    formal version

--- getsamplefile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget https://asiahpcgbb.blob.core.windows.net/gromacsonazure/synchost.sh 
-wget https://asiahpcgbb.blob.core.windows.net/gromacsonazure/rungmxsample.sh
-wget https://asiahpcgbb.blob.core.windows.net/gromacsonazure/benchMEM.tpr
-wget https://asiahpcgbb.blob.core.windows.net/gromacsonazure/nvt.tpr
-chmod a+x synchost.sh rungmxsample.sh
