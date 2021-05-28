43befaa2ec3c0bd0f9e6b82d156ecab542cae70c
iwillsky <45908192+Iwillsky@users.noreply.github.com>
2021-05-28 09:57:03 +0800

    Update0528
    upload

--- rungmxsample.sh
+++ rungmxsample.sh
@@ -10,7 +10,7 @@ srun bash -c 'sudo ./synchost.sh $HOSTLIST_FILENAME'
 module load mpi/openmpi
 
 #run sample 
-/opt/openmpi-4.0.5/bin/mpirun -np 2 --hostfile $HOSTLIST_FILENAME /usr/local/gromacs/bin/gmx_mpi mdrun -s /home/cyclecloud/benchMEM.tpr
+/opt/openmpi-4.0.5/bin/mpirun -np 3 --hostfile $HOSTLIST_FILENAME /usr/local/gromacs/bin/gmx_mpi mdrun -s /home/cyclecloud/benchMEM.tpr
 
 #another nvt sample 
-#/opt/openmpi-4.0.5/bin/mpirun -np 2 --hostfile $HOSTLIST_FILENAME /usr/local/gromacs/bin/gmx_mpi mdrun -v -deffnm nvt
+#/opt/openmpi-4.0.5/bin/mpirun -np 3 --hostfile $HOSTLIST_FILENAME /usr/local/gromacs/bin/gmx_mpi mdrun -v -deffnm nvt
