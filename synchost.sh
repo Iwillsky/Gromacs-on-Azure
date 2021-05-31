adf6e8ea2a194d372d49e0978175511956f83a38
iwillsky <45908192+Iwillsky@users.noreply.github.com>
2021-05-31 21:20:50 +0800

    Update synchost.sh

--- synchost.sh
+++ synchost.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-#SBATCH -o job%j.out
-#SBATCH --job-name=GmxMEMSample
-#SBATCH --nodes=3       
-#SBATCH --gres=gpu:1 
-
-export HOSTLIST_FILENAME=hostlist$(date "+%Y%m%d%H%M%S")
-srun bash -c 'sudo ./synchost.sh $HOSTLIST_FILENAME'
-
-module load mpi/openmpi
-
-#run sample 
-/opt/openmpi-4.0.5/bin/mpirun -np 3 --hostfile $HOSTLIST_FILENAME /usr/local/gromacs/bin/gmx_mpi mdrun -s benchMEM.tpr
-
-#another nvt sample 
-#/opt/openmpi-4.0.5/bin/mpirun -np 3 --hostfile $HOSTLIST_FILENAME /usr/local/gromacs/bin/gmx_mpi mdrun -v -deffnm nvt
+ThisNodename=`grep "SLURMD_OPTIONS=-N " /etc/sysconfig/slurmd | awk '{print $NF}'`
+Hostlistfile=$1
+echo $ThisNodename >> $Hostlistfile
